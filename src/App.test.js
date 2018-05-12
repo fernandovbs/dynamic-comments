773f14b408040a23dabf6c94206bb98d461db701
Fernando Souza <fernandostorm1@gmail.com>
2018-05-12 00:53:03 -0300

    Tests for App component added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import App from './App'
-import { shallow } from 'enzyme'
+import { shallow, mount, render } from 'enzyme'
+import { wrap } from 'module';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
     const wrapper = shallow(<App />)
+    expect(wrapper.length).toBe(1)
   })
+  
+  it('should have a container class', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.is('.container')).toBe(true)
+  })
+
+  it('Shows comments', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('Comments').length).toBe(1)
+  })  
+
+  it('Shows Comments', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('Comments').length).toBe(1)
+  })    
+
+  it('Shows NewComment', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('NewComment').length).toBe(1)
+  })      
 })

435e8752fc86c66a4b897a796d39f439832a27d5
Fernando Souza <fernandostorm1@gmail.com>
2018-05-12 19:20:48 -0300

    Added test coverage for the Comment component

---
+++ src/Comment.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {shallow, mount, render} from 'enzyme'
+import Comment from './components/Comment'
+
+describe('<Comment />', () =>{
+    const handleNewCommentMock = jest.fn()
+
+    it('renders without crashing', () => {
+        const wrapper = shallow(<Comment comment={'test'} />)
+        expect(wrapper.length).toBe(1) 
+        expect(wrapper.is('.card.card-body.bg-light')).toBe(true)
+    })
+})
